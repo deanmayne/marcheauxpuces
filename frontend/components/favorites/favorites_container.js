6fe7c4f08a21b7fec324562d2e5636c15d116641
Dean Mayne <deancmayne@gmail.com>
2020-12-15 13:35:38 -0800

    Minor bug fix

---
+++ frontend/components/favorites/favorites_container.js
@@ -0,0 +1,18 @@
+import { connect } from "react-redux";
+
+import FavoritesIndex from "./cart_index";
+
+const mapStateToProps = (state) => {
+  return {
+    reviews: Object.values(state.entities.favorites),
+    shopper_id: state.session.id,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FavoritesIndex);
